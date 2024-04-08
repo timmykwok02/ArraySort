fb9186dd62d510e1dad91bd45d96b5a3c5f23807
Timmy <timmykwok02@gmail.com>
2024-04-07 18:50:09 -0700

    Update arraySortTest.java

--- ArraySortTest/src/arraySortTest.java
+++ ArraySortTest/src/arraySortTest.java
@@ -3,18 +3,21 @@ import java.util.Arrays;
 public class arraySortTest {
 
 	public static void main(String[] args) {
-		// Sort integer array
+		// testing GitHub
 		int[] intArray = {4, 2, 3, 5, 1};
+		String[] stringArray = {"Hello", "Apple", "Cat"};
 		
-		System.out.println("Orginal Array");
+		System.out.println("Orginal Arrays");
 		System.out.println(Arrays.toString(intArray));
-
-		System.out.println("Sorted Array");
+		System.out.println(Arrays.toString(stringArray));
+		
+		System.out.println("Sorted Arrays");
 		intArray = sortInt(intArray);
 		System.out.println(Arrays.toString(intArray));
+		stringArray = sortString(stringArray);
+		System.out.println(Arrays.toString(stringArray));
 	}
 	
-	// insertion sort
 	public static int[] sortInt(int[] array) {
 		int num, index;
 		for (int x=1; x<array.length; x++) {
@@ -24,6 +27,24 @@ public class arraySortTest {
 			while (num<array[index]) {
 				array[index+1] = array[index];
 				array[index] = num;
+				index--;
+				if (index < 0)
+					break;
+			}
+		}
+		return array;
+	}
+	
+	public static String[] sortString(String[] array) {
+		String string;
+		int index;
+		for (int x=1; x<array.length; x++) {
+			string = array[x];
+			index = x-1;
+			
+			while (string.compareTo(array[index]) < 0) {
+				array[index+1] = array[index];
+				array[index] = string;
 				index--;
 				if (index < 0)
 					break;
